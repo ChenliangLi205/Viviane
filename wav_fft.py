54706bd76bc2e52f898a4ee847b5b7b164085777
RSGungnir <fanxuan158@gmail.com>
2018-07-15 19:21:04 +0800

    feat: add segment method to slice wave data

--- wav_fft.py
+++ wav_fft.py
@@ -25,18 +25,34 @@ def show(ori_func, rate):
 
 
 def readwav(path):
+    # 读wav文件，返回波形数据（1维数组）和帧率（整数）
     with wave.open(path, 'rb') as f:
         par = f.getparams()
         framerate, framenum = par[2], par[3]
         content = f.readframes(framenum)
         data = np.fromstring(content, dtype=np.int16)
         data.shape = -1, 2
-        data = data[2200:4200]  # 截取其中2000帧
         data = data.T
-        data = data[0]  # 只取左声道
+        data = data[0]  # 取单声道（0为左声道，1为右声道）
         return data, framerate
+
+
+def segment(data, span=4000):
+    # 传入整个乐曲的波形数据（1维数组），返回一个由分段组成的2维数组
+    # 每个分段都是长度固定为span的1维数组，并且仍是波形数据
+    # 注意span的单位是帧，不是毫秒，转换为时间单位需要除以帧率
+    # 由于data的长度不一定是span的整数倍，需要在data末尾用0填充至整数倍，以保证最后一个分段的长度仍为span
+    n = len(data)
+    if n % span:
+        lack = span - n % span
+        complete = np.concatenate((data, np.zeros(lack, dtype=np.int16)))
+    else:
+        complete = data.copy()
+    return complete.reshape(-1, span)
 
 
 if __name__ == '__main__':
     wavdata, wavrate = readwav(r'28-C.wav')
-    show(wavdata, wavrate)
+    seg = segment(wavdata)
+    show(seg[0], wavrate)
+    show(seg[1], wavrate)
